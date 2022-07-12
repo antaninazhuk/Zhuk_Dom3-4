0af0fc6b603611fe925cce6f3be61e44dc61e792
antaninazhuk <101988172+antaninazhuk@users.noreply.github.com>
2022-07-12 22:31:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Task 1
+
+// document.getElementById('seach').addEventListener('focus', function(e) {
+//     console.log("Фокус");
+//     setTimeout(() => {
+//         this.blur();
+//     }, 5000);
+// })
+
+
+// Task 2
+
+// form = document.forms.main;
+// form.addEventListener("focus", function(e) {
+//     this.classList.add('active')
+
+// }, true);
+
+// form.addEventListener("blur", function(e) {
+//     this.classList.remove('active')
+
+// }, true);
+
+// Task 3
+
+// params = {
+//     model: 'iPhone 12 Pro',
+//     version: 15.5,
+//     color: 'blue'
+// }
+
+// localStorage.setItem('phone', JSON.stringify(params));
+
+// myName = "Antanina";
+// mySurname = "Zhuk";
+
+// localStorage.setItem('name', myName);
+// localStorage.setItem('surname', mySurname);
+// localStorage.removeItem('name');
